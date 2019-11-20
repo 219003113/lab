fdc27bcc8cbd45f9ae40a31b4031405da304c4bd
219003113 <42488276+219003113@users.noreply.github.com>
2019-11-20 10:41:42 +0530

    custom fied

---
+++ js/companyfield.js
@@ -0,0 +1,15 @@
+$zoho.salesiq.ready = function()
+
+{
+   $zoho.salesiq.customfield.add( 
+{ 
+   "name" :"Company", 
+   "hint" :"Company", 
+   "required" :"false", 
+   "maxlength" :"50", 
+   "minlength" :"3",
+   "type" :"textarea", 
+   "visibility" :"both"
+});
+
+}
